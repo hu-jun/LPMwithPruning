6da066a5089937a8aad38c45471e73d798a2b82b
hu-jun <hujunvscomputer@163.com>
2016-04-18 10:08:54 +0800

    main.cpp modify

--- main.cpp
+++ main.cpp
@@ -8,15 +8,19 @@ int main()
 {
     cout << "starting!" << endl;
 
-	string randomIp_route_table_file = "../data4test/randomIP/route_table.tr";
-	string randomIp_query_file = "../data4test/randomIP/random_flow.tr";
+	string randomIp_route_table_file = "../data4test/randomIP/route_table.tr";  //file path of route table to test random traffic
+	string randomIp_query_file = "../data4test/randomIP/random_flow.tr";        //file path of random traffic
+	int pivotLevel1 = 24;  // # of level to do pivot
 
-    randomIpTest(randomIp_route_table_file, randomIp_query_file, 24);
+    randomIpTest(randomIp_route_table_file, randomIp_query_file, pivotLevel1);//Random ip test
 
-	string fibIp_route_table_file = "../data4test/fibIp/route_table.tr";
-	string fibIp_query_file = "../data4test/fibIp/fib_flow.tr";
+//------------------------------------------------------------------------------------------------------
 
-    fibIpTest(fibIp_route_table_file, fibIp_query_file, 24);     //fib ip test
+	string fibIp_route_table_file = "../data4test/fibIp/route_table.tr";  //file path of route table to test prefix-based traffic
+	string fibIp_query_file = "../data4test/fibIp/fib_flow.tr";           //file path of prefix-based traffic
+	int pivotLevel2 = 24;  // # of level to do pivot
+
+    fibIpTest(fibIp_route_table_file, fibIp_query_file, pivotLevel2);     //prefix-based traffic test
 
     return 0;
 }
